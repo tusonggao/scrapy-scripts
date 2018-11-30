14931ad67e12c59af1e23c2fe2f65fe466f6d6e4
涂松高 <tusonggao@163.com>
2018-11-30 17:34:28 +0800

    add new project chinanews_crawler

---
+++ chinanews_crawler/chinanews_crawler/spiders/ChinaNewsSpider.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import Spider
+from scrapy.http import Request
+from ..items import NewsFeedItem
+from bs4 import BeautifulSoup
+
+
+class ChinaNewsSpider(Spider):
+    name = 'chinanews'
+    allowed_domains = ['chinanews.com']
+    start_urls = (
+    	'http://http://www.chinanews.com/rss/rss_2.html',
+    )
+
+    def parse(self, response):
+        rss_page = BeautifulSoup(response.body, 'html.parser')
+        rss_links = set([item['href'] for item in rss_page.find_all('a')])
+        for link in rss_links:
+        	yield Request(url=link, callback=self.parse_feed)
+
+    def parse_feed(self, response):
+    	rss = BeautifulSoup(response.body, 'lxml')
+    	for item in rss.find_all('item'):
+    		feed_item = NewsFeedItem()
+    		feed_item['title'] = item.title.text
+    		feed_item['link'] = item.link.text
+    		feed_item['desc'] = item.description.text
+    		feed_item['pub_date'] = item.pubdate.text
+
+    		yield feed_item
