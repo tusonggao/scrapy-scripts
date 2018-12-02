2dd97c0a55e02c51a43c7a335c91ac64d8bbaf1b
涂松高 <tusonggao@163.com>
2018-12-02 09:57:03 +0800

    update mobiles.py

--- amazon/amazon/spiders/mobile.py
+++ amazon/amazon/spiders/mobile.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+# https://www.bilibili.com/video/av30493305
+
 
 class MobileSpider(scrapy.Spider):
     name = 'mobile'
@@ -8,6 +10,7 @@ class MobileSpider(scrapy.Spider):
     start_urls = ['https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=mobile+phone']
 
     def parse(self, response):
-        print(response.body)
-        titles = response.xpath('//ul[@class="s-results-list-atf"]/li//h2/text()').extract()
+        print('response.url is ', response.url)
+        titles = response.xpath('//ul[@id="s-results-list-atf"]/li//h2/text()').extract()
         print('titles is ', titles)
+        print('len of titles is', len(titles))
